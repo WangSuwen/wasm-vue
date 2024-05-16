6099dfb4f39253d292c349d39fec48ae351e536c
wangsuwen <1002853070@qq.com>
2024-05-16 17:59:42 +0800

    添加：模拟键盘点击的rust代码

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -1,13 +1,17 @@
 extern crate wasm_bindgen;
-
 use wasm_bindgen::prelude::*;
-
+use enigo::{Enigo, Key, Settings, Direction};
+use enigo::Keyboard;
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("你好呀, 我是WebAssembly ++--===--++ {}!", name));
+pub fn greet(/* name: &str */) {
+    // alert(&format!("你好呀, 我是WebAssembly ++--===--++ {}!", name));
+    alert(&format!("你好呀, 我是WebAssembly"));
+	let enigo = enigo::Enigo::new(&Settings::default());
+    let mut eni = enigo.unwrap();
+    let _ = eni.key(Key::Unicode('v'), Direction::Press);
 }
